54b7913ef165deb0f05b104551bf247b1111e075
JadenFicklin <jadendficklin@gmail.com>
2022-12-05 15:30:35 -0700

    styling

--- src/components/RandomQuote.js
+++ src/components/RandomQuote.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "../styles/RandomQuote.css";
+import { FaQuoteLeft } from "react-icons/fa";
+import { FaTumblr } from "react-icons/fa";
+import { BsTwitter } from "react-icons/bs";
 
 function RandomQuote() {
   return (
@@ -7,17 +10,19 @@ function RandomQuote() {
       <div className="quote-outer">
         <div className="quote-box">
           <div className="quote-text">
+            <FaQuoteLeft className="quote-icon" />
             Go confidently in the direction of your dreams. Live the life you
             have imagined.
           </div>
           <div className="quote-author">- Henry David Thoreau</div>
           <div className="quote-buttons">
-            <div className="quote-twitter">Tw</div>
-            <div className="quote-tumblr">Tu</div>
-            <div className="new-quote">
-              git
-              <button>New quote</button>
+            <div className="quote-twitter">
+              <BsTwitter className="icon" />
             </div>
+            <div className="quote-tumblr">
+              <FaTumblr className="icon" />
+            </div>
+            <div className="new-quote">New quote</div>
           </div>
         </div>
       </div>
