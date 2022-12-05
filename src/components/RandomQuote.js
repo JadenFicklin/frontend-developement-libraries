1e532813075589870ffc43fd858ea6d1532005f4
JadenFicklin <jadendficklin@gmail.com>
2022-12-05 13:55:15 -0700

    added in basics for quote box

--- src/components/RandomQuote.js
+++ src/components/RandomQuote.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import "../styles/RandomQuote.css";
 
 function RandomQuote() {
   return (
-    <div>RandomQuote</div>
-  )
+    <>
+      <div className="quote-outer">
+        <div className="quote-box">
+          <div className="quote-text">
+            Go confidently in the direction of your dreams. Live the life you
+            have imagined.
+          </div>
+          <div className="quote-author">- Henry David Thoreau</div>
+          <div className="quote-buttons">
+            <div className="quote-twitter">Tw</div>
+            <div className="quote-tumblr">Tu</div>
+            <div className="new-quote">
+              git
+              <button>New quote</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default RandomQuote
+export default RandomQuote;
